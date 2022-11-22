07394e8cfac244d318473345af1cb48282d61d21
Niharika Upadhyay <36950265+Niharika0806@users.noreply.github.com>
2022-11-22 13:21:12 +0000

    Added sample test case

--- test/environment_unittest.py
+++ test/environment_unittest.py
@@ -1,11 +1,20 @@
-from unittest import TestCase
+import unittest
+
+from Environment import Environment
 
 
-class EnvironmentTest(TestCase):
+class EnvironmentTest(unittest.TestCase):
     # TODO complete test
     def test_correct_rotate_car(self):
-        self.assertTrue(True)
+        expected_matrix = [[-19, 40],[20,40], [19, -40], [-20, -40]]
+        input_matrix = [[40,20], [40, -20], [-40, -20], [-40, 20]]
+        output_matrix = Environment.rotate_car(input_matrix, 90)
+        # self.assertEqual(EXPECTED_OUTPUT, output_MATRIX)
 
     # TODO complete test
     def test_incorrect_rotate_car(self):
         self.assertTrue(False)
+
+
+if __name__ == '__main__':
+    unittest.main()
