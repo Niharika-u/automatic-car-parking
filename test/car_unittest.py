f94db2dcd91be131d1894f76829a8d90b47ae222
Niharika Upadhyay <36950265+Niharika0806@users.noreply.github.com>
2022-11-25 19:38:16 +0000

    Updated unit tests

--- test/car_unittest.py
+++ test/car_unittest.py
@@ -1,114 +1,44 @@
-#import sys
-#sys.path.insert(0, '/Users/shubh/automatic-car-parking')
+# import sys
+# sys.path.insert(0, '/Users/shubh/automatic-car-parking')
 import unittest
 import numpy as np
 
-
-import argparse
 from Car import determine_empty_spot
 from Car import get_parking_coordinates
 
 from ParkingLot import ParkingLot
-   
-    
-    
-class car_unittest((unittest.TestCase)):  
+
+
+class car_unittest(unittest.TestCase):
     def test_correct_determine_empty_spot(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--x_start', type=int, default=50, help='X of start')
-        parser.add_argument('--y_start', type=int, default=0, help='Y of start')
-        parser.add_argument('--parking_spot', type=list, default=[5], help='empty car position in parking out of 12')
-        args = parser.parse_args()
-
-        pl = ParkingLot(args.parking_spot)
-
+        pl = ParkingLot([5])
         parkingenv = pl.get_cars()
         expected_emptyspots = np.array([5])
-        output_expeted = determine_empty_spot(parkingenv)
-        self.assertEqual(expected_emptyspots, output_expeted)
+        output_expected = determine_empty_spot(parkingenv)
+        self.assertEqual(expected_emptyspots, output_expected)
 
     def test_incorrect_determine_empty_spot(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--x_start', type=int, default=50, help='X of start')
-        parser.add_argument('--y_start', type=int, default=0, help='Y of start')
-        parser.add_argument('--parking_spot', type=list, default=[6], help='empty car position in parking out of 12')
-        args = parser.parse_args()
-
-        pl = ParkingLot(args.parking_spot)
-
+        pl = ParkingLot([6])
         parkingenv = pl.get_cars()
         expected_emptyspots = np.array([5])
-        output_expeted = determine_empty_spot(parkingenv)
-        self.assertNotEqual(expected_emptyspots, output_expeted)
+        output_expected = determine_empty_spot(parkingenv)
+        self.assertNotEqual(expected_emptyspots, output_expected)
 
     def test_correct_left_get_parking_coordinates(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--x_start', type=int, default=50, help='X of start')
-        parser.add_argument('--y_start', type=int, default=0, help='Y of start')
-        parser.add_argument('--parking_spot', type=list, default=[5], help='empty car position in parking out of 12')
-        args = parser.parse_args()
-
-        pl = ParkingLot(args.parking_spot)
-
-        parkingenv = pl.get_cars()
-        emptySpots = determine_empty_spot(parkingenv)
-
-        cars = {1: [[45, 20]], 2: [[65, 20]],
-        3: [[35, 32]], 4: [[65, 32]],
-        5: [[35, 44]], 6: [[65, 44]],
-        7: [[35, 56]], 8: [[65, 56]],
-        9: [[35, 68]], 10: [[65, 68]],
-        11: [[35, 80]], 12: [[65, 80]]}
-
-        expected = (35,56)
+        expected = (35, 56)
         output = get_parking_coordinates()
         self.assertEqual(expected, output)
 
     def test_correct_right_get_parking_coordinates(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--x_start', type=int, default=50, help='X of start')
-        parser.add_argument('--y_start', type=int, default=0, help='Y of start')
-        parser.add_argument('--parking_spot', type=list, default=[6], help='empty car position in parking out of 12')
-        args = parser.parse_args()
-
-        pl = ParkingLot(args.parking_spot)
-
-        parkingenv = pl.get_cars()
-        emptySpots = determine_empty_spot(parkingenv)
-
-        cars = {1: [[45, 20]], 2: [[65, 20]],
-        3: [[35, 32]], 4: [[65, 32]],
-        5: [[35, 44]], 6: [[65, 44]],
-        7: [[35, 56]], 8: [[65, 56]],
-        9: [[35, 68]], 10: [[65, 68]],
-        11: [[35, 80]], 12: [[65, 80]]}
-
-        expected = (35,56)
+        expected = (35, 56)
         output = get_parking_coordinates()
         self.assertEqual(expected, output)
 
-    #def test_incorrect_get_parking_coordinates(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--x_start', type=int, default=50, help='X of start')
-        parser.add_argument('--y_start', type=int, default=0, help='Y of start')
-        parser.add_argument('--parking_spot', type=list, default=[6], help='empty car position in parking out of 12')
-        args = parser.parse_args()
-
-        pl = ParkingLot(args.parking_spot)
-
-        parkingenv = pl.get_cars()
-        emptySpots = []
-
-        cars = {1: [[45, 20]], 2: [[65, 20]],
-        3: [[35, 32]], 4: [[65, 32]],
-        5: [[35, 44]], 6: [[65, 44]],
-        7: [[35, 56]], 8: [[65, 56]],
-        9: [[35, 68]], 10: [[65, 68]],
-        11: [[35, 80]], 12: [[65, 80]]}
-
+    def test_incorrect_get_parking_coordinates(self):
         expected = 0
         output = get_parking_coordinates()
         self.assertNotEqual(expected, output)
+
 
 if __name__ == '__main__':
     unittest.main()
