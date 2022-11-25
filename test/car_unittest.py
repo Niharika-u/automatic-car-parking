f5e4f8c027d16a66e1782fb2102d1e5455bf46aa
Smit VaghelaUCC <122112348@umail.ucc.ie>
2022-11-25 20:24:20 +0000

    pull last

--- test/car_unittest.py
+++ test/car_unittest.py
@@ -1,5 +1,5 @@
-# import sys
-# sys.path.insert(0, '/Users/shubh/automatic-car-parking')
+import sys
+sys.path.insert(0, '/Users/smitvaghela/Desktop/CS/automatic-car-parking')
 import unittest
 import numpy as np
 
@@ -39,6 +39,29 @@ class car_unittest((unittest.TestCase)):
         expected = (35, 56)
         output = get_parking_coordinates()
         self.assertEqual(expected, output)
+
+    def test_incorrect_get_parking_coordinates(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--x_start', type=int, default=50, help='X of start')
+        parser.add_argument('--y_start', type=int, default=0, help='Y of start')
+        parser.add_argument('--parking_spot', type=list, default=[6], help='empty car position in parking out of 12')
+        args = parser.parse_args()
+
+        pl = ParkingLot(args.parking_spot)
+
+        parkingenv = pl.get_cars()
+        emptySpots = [5]
+
+        cars = {1: [[45, 20]], 2: [[65, 20]],
+                3: [[35, 32]], 4: [[65, 32]],
+                5: [[35, 44]], 6: [[65, 44]],
+                7: [[35, 56]], 8: [[65, 56]],
+                9: [[35, 68]], 10: [[65, 68]],
+                11: [[35, 80]], 12: [[65, 80]]}
+
+        expected = 0
+        output = get_parking_coordinates()
+        self.assertNotEqual(expected, output)
 
 
 if __name__ == '__main__':
