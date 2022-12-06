b5d81b4504a79bc41d8c5d37d034e01febbae91f
Niharika Upadhyay <36950265+Niharika0806@users.noreply.github.com>
2022-12-06 18:35:44 +0000

    Update config.py

--- config.py
+++ config.py
@@ -2,25 +2,25 @@ car_length = 80
 small_cars = [3, 10, 9, 7]
 
 # Right parking
-# empty_spots = [4, 9, 11]
+empty_spots = [4, 9, 11]
 
 # Left Parking
-empty_spots = [3, 7]
-
+# empty_spots = [3, 7]
+#
 # No Parking
 # empty_spots = [3]
 
 # Enough space
-# big_cars = [1, 3, 14]
+big_cars = [1, 3, 14]
 
 # Not Enough space
-big_cars = [1, 5, 3, 14]
+# big_cars = [1, 5, 3, 14]
 
 
 cars = {1: [[35, 20]], 2: [[65, 20]],
         3: [[35, 32]], 4: [[65, 32]],
-        # 5: [[35, 44]], 6: [[65, 44]],  # Enough Space
-        5: [[35, 41]], 6: [[65, 44]],  # Not enough space
+        5: [[35, 44]], 6: [[65, 44]],  # Enough Space
+        # 5: [[35, 41]], 6: [[65, 44]],  # Not enough space
         7: [[35, 56]], 8: [[65, 56]],
         9: [[35, 68]], 10: [[65, 68]],
         11: [[35, 80]], 12: [[65, 80]],
